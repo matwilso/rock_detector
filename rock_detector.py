fc519d9d828241e4d8e3a20e70fe927cda1898b6
matwilso <mattwilsonmbw@gmail.com>
2017-11-15 12:38:10 -0700

    move argparser code to lower. update lfs

--- rock_detector.py
+++ rock_detector.py
@@ -1,58 +1,13 @@
 #!/usr/bin/env python3
 
+import os
 import argparse
 import itertools
 import numpy as np
 import tensorflow as tf
 from arena_modder import ArenaModder
-
 from utils import display_image
 
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument(
-    '--train_epochs', type=int, default=100,
-    help='The number of epochs to use for training.')
-
-parser.add_argument(
-    '--epochs_per_eval', type=int, default=1,
-    help='The number of training epochs to run between evaluations.')
-
-parser.add_argument(
-    '--batch_size', type=int, default=16,
-    help='Batch size for training and evaluation.')
-
-parser.add_argument(
-    '--visualize', type=bool, default=False,
-    help="Don't train, just interact with the mujoco sim and visualize everything")
-
-parser.add_argument(
-    '--super_batch', type=int, default=1000,
-    help='Number of batches before generating new rocks')
-
-parser.add_argument(
-    '--save_path', type=str, default="net.weights",
-    help='File to save weights to')
-
-parser.add_argument(
-    '--save_every', type=int, default=1,
-    help='Number of batches before saving weights')
-
-parser.add_argument(
-    '--load_path', type=str, default="net.weights",
-    help='File to load weights from')
-
-parser.add_argument(
-    '--dtype', type=str, default="cpu",
-    help='cpu or gpu')
-
-parser.add_argument(
-    '--blender_path', type=str, default="blender", help='Path to blender executable')
-
-parser.add_argument(
-    '--os', type=str, default="none",
-    help='none (don\'t override any defaults) or mac or ubuntu')
 
 # NOTE: 2^2 * 1k images should get decent convergence (about ~4k, ~64k should be bomb)
 # could be about 2 days for full convergence
@@ -61,7 +16,6 @@ parser.add_argument(
 
 # TODO: I should try to run this with and without the floor randomization and see if it
 # still works as well.  My guess is that with floor it will still be fine
-
 
 def arena_sampler():
     """
@@ -87,40 +41,81 @@ def arena_sampler():
         if i % FLAGS.super_batch == 0:
             arena_modder.randrocks()
 
-
 def main():
     x_batch = []
     y_batch = []
     x_frames = []
     y_grounds = []
-    batch_count = 0
-    last_batch_count = 0
-
-
     sampler = arena_sampler()
-    while True:
+
+    for i in itertools.count(1):
         batch_imgs = []
         batch_ground_truths = []
-        for i in range(FLAGS.batch_size):
+        for _ in range(FLAGS.batch_size):
             cam_img, rock_ground_truth = next(sampler)
-
-            # TODO: test if we can do this batched
 
             batch_imgs.append(cam_img)
             batch_ground_truths.append(rock_ground_truth)
 
-
         cam_imgs = np.stack(batch_imgs)
         ground_truths = np.stack(batch_ground_truths)
         
-        print(cam_imgs.shape)
-        print(ground_truths.shape)
+        _, curr_loss, curr_pred_output, summary  = sess.run([train, loss, pred_output, merged], {img_input : cam_imgs, real_output : ground_truths})
 
-        _, curr_loss = sess.run([train, loss], {img_input : cam_imgs, real_output : ground_truths})
-        print(curr_loss)
+        if i % 1 == 0:
+            test_writer.add_summary(summary, i)
+        if i % FLAGS.save_every == 0:
+            save_path = saver.save(sess, FLAGS.save_path)
+            print("Model saved in file: %s" % save_path)
+
+        #print(ground_truths)
+        #print(curr_pred_output)
+        #print(curr_loss)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--log_dir', type=str,
+        default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),'tensorflow/rock_detector'),
+        help="Directory to log data for TensorBoard to")
+    parser.add_argument(
+        '--log_every', type=int, default=100,
+        help='Number of batches to run before logging data')
+    parser.add_argument(
+        '--train_epochs', type=int, default=100,
+        help='The number of epochs to use for training.')
+    parser.add_argument(
+        '--epochs_per_eval', type=int, default=1,
+        help='The number of training epochs to run between evaluations.')
+    parser.add_argument(
+        '--batch_size', type=int, default=16,
+        help='Batch size for training and evaluation.')
+    parser.add_argument(
+        '--visualize', type=bool, default=False,
+        help="Don't train, just interact with the mujoco sim and visualize everything")
+    parser.add_argument(
+        '--super_batch', type=int, default=1000,
+        help='Number of batches before generating new rocks')
+    parser.add_argument(
+        '--save_path', type=str, default="weights/model.ckpt",
+        help='File to save weights to')
+    parser.add_argument(
+        '--save_every', type=int, default=100,
+        help='Number of batches before saving weights')
+    parser.add_argument(
+        '--load_path', type=str, default="weights/model.ckpt",
+        help='File to load weights from')
+    parser.add_argument(
+        '--dtype', type=str, default="cpu",
+        help='cpu or gpu')
+    parser.add_argument(
+        '--blender_path', type=str, default="blender", help='Path to blender executable')
+    parser.add_argument(
+        '--os', type=str, default="none",
+        help='none (don\'t override any defaults) or mac or ubuntu')
+
+
     # Parse command line arguments
     FLAGS, unparsed = parser.parse_known_args()
     if FLAGS.os == "mac":
@@ -134,6 +129,11 @@ if __name__ == "__main__":
         pass
     else:
         pass
+
+    # Delete and remake log dir if it already exists 
+    if tf.gfile.Exists(FLAGS.log_dir):
+      tf.gfile.DeleteRecursively(FLAGS.log_dir)
+    tf.gfile.MakeDirs(FLAGS.log_dir)
 
     # Arena modder setup
     arena_modder = ArenaModder("xmls/nasa/box.xml", blender_path=FLAGS.blender_path, visualize=FLAGS.visualize)
@@ -162,10 +162,28 @@ if __name__ == "__main__":
     # optimizer
     optimizer = tf.train.AdamOptimizer(1e-4) # 1e-4 suggested from dom rand paper
     train = optimizer.minimize(loss)
-    
-    init = tf.global_variables_initializer()
-    sess = tf.Session()
-    sess.run(init)
 
+
+
+    tf.summary.image('input', img_input, 30)
+    tf.summary.scalar('loss', loss)
+    tf.summary.histogram('loss_histogram', loss)
+    merged = tf.summary.merge_all()
+
+    sess = tf.Session()
+    train_writer = tf.summary.FileWriter(FLAGS.log_dir + '/train', sess.graph)
+    test_writer = tf.summary.FileWriter(FLAGS.log_dir + '/test')
+
+    
+    # Add ops to save and restore all the variables.
+    saver = tf.train.Saver()
+
+    if FLAGS.load_path != "0":
+        saver.restore(sess,  FLAGS.load_path)
+        print("Model restored.")
+    else:
+        # Initialize all variables
+        init = tf.global_variables_initializer()
+        sess.run(init)
 
     main()
